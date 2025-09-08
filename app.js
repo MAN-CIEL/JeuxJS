2da507665c89871ed9aec4a8f094bbce66437626
Ilan Manzari <ilanmanzari2607@gmail.com>
2025-09-08 16:16:57 +0200

    Commit #2

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 'use strict';
-
-console.log('TP CIEL');
+var express = require('express');
+var exp = express();
+exp.use(express.static(__dirname + '/www'));
+exp.get('/', function (req, res) {
+    console.log("Réponse à un client");
+    res.sendFile(__dirname + '/www/index.html');
+});
+exp.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Erreur serveur express');
+});
+var port = 80;
+exp.listen(port, function () { console.log('Serveur en écoute'); });
