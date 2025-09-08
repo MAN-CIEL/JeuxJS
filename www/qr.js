3b798d2f301d7b5c927fe309cf238404f859292c
Ilan Manzari <ilanmanzari2607@gmail.com>
2025-09-08 17:57:39 +0200

    Commit #6

--- www/qr.js
+++ www/qr.js
@@ -1,56 +1,40 @@
-var ipServeur = location.hostname;  // Adresse IP ou nom du serveur
-var ws;                             // WebSocket client
+var question = '?';
+var bonneReponse = 0;
 
-window.onload = function () {
-    if ('WebSocket' in window) {
-        ConnexionAuServeurWebsocket();
-    } else {
-        alert('WebSocket non supporté par le navigateur');
-    }
-    ControleIHM();
-};
+// Connexion des clients a la WebSocket /qr et evenements associés 
+// Questions/reponses 
+exp.ws('/qr', function (ws, req) {
+    console.log('Connection WebSocket %s sur le port %s',
+        req.connection.remoteAddress, req.connection.remotePort);
+    NouvelleQuestion();
 
-// Fonction pour ouvrir la connexion WebSocket
-function ConnexionAuServeurWebsocket() {
-    ws = new WebSocket('ws://' + ipServeur + '/qr');
+    ws.on('message', TraiterReponse);
 
-    ws.onopen = function () {
-        console.log('WebSocket ouverte');
-    };
+    ws.on('close', function (reasonCode, description) {
+        console.log('Deconnexion WebSocket %s sur le port %s',
+            req.connection.remoteAddress, req.connection.remotePort);
+    });
 
-    ws.onclose = function () {
-        alert('Connexion WebSocket fermée');
-    };
 
-    ws.onmessage = function (evt) {
-        var mess = JSON.parse(evt.data);
-        document.getElementById('question').textContent = mess.question;
-        document.getElementById('resultats').textContent = JSON.stringify(mess.joueurs, null, 2);
-    };
-}
-
-// Gérer le clic sur le bouton Valider
-function ControleIHM() {
-    document.getElementById('Valider').onclick = BPValider;
-}
-
-// Envoyer la réponse du joueur au serveur
-function BPValider() {
-    var nom = document.getElementById('nom').value.trim();
-    var reponse = document.getElementById('reponse').value.trim();
-
-    if (nom === '') {
-        alert('Veuillez saisir votre nom');
-        return;
+    function TraiterReponse(message) {
+        console.log('De %s %s, message :%s', req.connection.remoteAddress,
+            req.connection.remotePort, message);
+        if (message == bonneReponse) {
+            NouvelleQuestion();
+        }
     }
 
-    if (reponse === '') {
-        alert('Veuillez saisir une réponse');
-        return;
+
+    function NouvelleQuestion() {
+        var x = GetRandomInt(11);
+        var y = GetRandomInt(11);
+        question = x + '*' + y + ' =  ?';
+        bonneReponse = x * y;
+        aWss.broadcast(question);
     }
 
-    var message = JSON.stringify({ nom: nom, reponse: reponse });
-    ws.send(message);
+    function GetRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
 
-    document.getElementById('reponse').value = '';  // Effacer le champ réponse
-}
+}); 
