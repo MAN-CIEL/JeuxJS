b7fc3a42241394f5fcef194333a45951944c028b
Ilan Manzari <ilanmanzari2607@gmail.com>
2025-09-15 14:10:16 +0200

    Commit #7

--- www/qr.js
+++ www/qr.js
@@ -37,4 +37,25 @@ exp.ws('/qr', function (ws, req) {
         return Math.floor(Math.random() * Math.floor(max));
     }
 
-}); 
+    function TraiterReponse(wsClient, message) {
+        var mess = JSON.parse(message);
+        // ... gestion du joueur ...
+
+        if (mess.reponse == this.bonneReponse) {
+            // Envoyer "Bonne réponse !" au joueur ayant répondu
+            wsClient.send(JSON.stringify({ question: "Bonne réponse !" }));
+            // Après 3 secondes, envoyer une nouvelle question à tous
+            setTimeout(() => {
+                this.NouvelleQuestion();
+            }, 3000);
+        } else {
+            // Envoyer "Mauvaise réponse" uniquement au joueur
+            wsClient.send(JSON.stringify({ question: "Mauvaise réponse" }));
+            // Après 3 secondes, redisplay la question (ou nouvelle question selon règles)
+            setTimeout(() => {
+                wsClient.send(JSON.stringify({ question: this.question }));
+            }, 3000);
+        }
+    }
+
+});
